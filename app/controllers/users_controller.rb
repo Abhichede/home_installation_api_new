f35df39556f2f3f4be697cb6cf7d0fbfc61eb4a4
Abhijit Chede <abhijit.chede@techverito.com>
2022-01-15 21:57:40 +0530

    Changes the response for create user API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: {id: @user.id, created_at: @user.created_at}, status: :ok, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
